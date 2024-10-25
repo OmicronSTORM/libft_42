80b13d2df3b49cb21790d56c01823cae8d5f82ce
Justin Owoundi Nlate <jowoundi@fu-r9-p3.s19.be>
2024-10-26 01:27:34 +0200

    save

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: jowoundi <jowoundi@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/25 18:37:45 by jowoundi          #+#    #+#             */
-/*   Updated: 2024/10/25 21:14:25 by jowoundi         ###   ########.fr       */
+/*   Updated: 2024/10/26 01:20:42 by jowoundi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,15 +15,31 @@
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	size_t	i;
+	size_t	j;
 	char	*new_string;
 
 	i = 0;
+	j = start;
 	if(!s)
 		return (0);
-	while (start != ft_strlen(s))
-		i++;
-	while (s[i] && i < len)
+	if (j > ft_strlen((char *)s))
+		return (ft_strdup(""));
+	if (len > ft_strlen((char *)s + j))
+		len = (ft_strlen((char *)s + j));
+	new_string = malloc(sizeof(char *) * (len + 1));
+	if (!new_string)
+		return (0);
+	while (i < len)
 	{
-		
+		new_string[i] = s[j + i];
+		i++;
 	}
+	new_string[j] = '\0';
+	return(new_string);
+}
+
+int	main()
+{
+	printf("%s\n", ft_substr("Bien le bonjour", 0, 10));
+	return(0);
 }
