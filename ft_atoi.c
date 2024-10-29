77071ff93593f25f6956bc272347c1aaa679ce4e
Justin Owoundi Nlate <jowoundi@fu-r9-p3.s19.be>
2024-10-29 17:20:01 +0100

    save

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: jowoundi <jowoundi@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/10 18:40:11 by jowoundi          #+#    #+#             */
-/*   Updated: 2024/10/24 19:46:02 by jowoundi         ###   ########.fr       */
+/*   Updated: 2024/10/29 17:14:21 by jowoundi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,6 @@ int	ft_atoi(char *str)
 	int	sign;
 
 	i = 0;
-	res = 0;
 	sign = 1;
 	while (str[i] == 32 || (str[i] >= 9 && str[i] <= 13))
 		i++;
@@ -34,4 +33,19 @@ int	ft_atoi(char *str)
 		i++;
 	}
 	return (sign * res);
+}
+
+#include <stdio.h>
+#include <stdlib.h>
+int	main(int ac, char **av)
+{
+	int	i = 1;
+
+	(void)ac;
+	while (av[i])
+	{
+		printf("Mon atoi: %d\n", ft_atoi(av[i]));
+		printf("VRAI atoi: %d\n", atoi(av[i]));
+		i++;
+	}
 }
